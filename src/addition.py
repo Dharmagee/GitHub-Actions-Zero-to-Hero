1e2ffeaa41a82b6c08e9870b840a8926ea880e56
Dharmagee <36490719+Dharmagee@users.noreply.github.com>
2024-02-19 22:27:34 -0500

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,10 +1,17 @@
 # app.py
 # This is a test commit
-# New Commit
+# New function for multiplication
 
 def add(a, b):
     return a + b
 
+def mul(c, d):
+    return c * d
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+def test_mul():
+    assert mul(2, 2) == 4
+    
